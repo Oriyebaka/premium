b34c6b95d008a1fe0aaada7ec7864652705c69a1
Oriyebaka <45126220+Oriyebaka@users.noreply.github.com>
2025-05-05 23:03:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -296,3 +296,40 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 })
 
+
+                var url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?9021';
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = url;
+                var options = {
+                "enabled":true,
+                "chatButtonSetting":{
+                    "backgroundColor":"#00e785",
+                    "ctaText":"Chat with us",
+                    "borderRadius":"25",
+                    "marginLeft": "0",
+                    "marginRight": "20",
+                    "marginBottom": "20",
+                    "ctaIconWATI":false,
+                    "position":"right"
+                },
+                "brandSetting":{
+                    "brandName":"Premium Preversity",
+                    "brandSubTitle":"undefined",
+                    "brandImg":"https://www.premiumpreversityng.com/images/logo.png",
+                    "welcomeText":"Hi there!\nHow can I help you?",
+                    "messageText":"",
+                    "backgroundColor":"#00e785",
+                    "ctaText":"Chat with us",
+                    "borderRadius":"25",
+                    "autoShow":false,
+                    "phoneNumber":"2347067866444"
+                }
+                };
+                s.onload = function() {
+                    CreateWhatsappChatWidget(options);
+                };
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+          
